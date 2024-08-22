65b792409f4ce08d4faeb4438511877507fdbf6a
Rendell Eyas <rendell.eyas@gmail.com>
2024-08-22 15:03:58 +0800

    up: initial module implementation

---
+++ app/Filament/Resources/ModuleResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ModuleResource\Pages;
+use App\Models\Module;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ModuleResource extends Resource
+{
+    protected static ?string $model = Module::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Module')
+                    ->schema([
+                        TextInput::make('name')->label('Name')
+                            ->required(),
+                        Textarea::make('description')
+                            ->required(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListModules::route('/'),
+            'create' => Pages\CreateModule::route('/create'),
+            'edit' => Pages\EditModule::route('/{record}/edit'),
+        ];
+    }
+}
